c0810ce3f09b7a6df119e9536ba518c13e85f52d
Josh Rittenberry <141363044+JoshRittenberry@users.noreply.github.com>
2024-01-10 09:42:37 -0600

    Classes almost complete

--- CornerStore/Models/OrderProduct.cs
+++ CornerStore/Models/OrderProduct.cs
@@ -2,4 +2,13 @@ namespace CornerStore.Models;
 
 public class OrderProduct
 {
+    public int Id { get; set; }
+    [Required]
+    public int ProductId { get; set; }
+    public OrderProduct Product { get; set; }
+    [Required]
+    public int OrderId { get; set; }
+    public Order Order { get; set; }
+    [Required]
+    public int Quantity { get; set; }
 }
